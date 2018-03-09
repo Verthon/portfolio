92e69b82a65569af9b83a34fc13d7150392e0e52
Verthon <krzysiekksordyl@gmail.com>
2018-03-09 10:46:04 +0100

    restructured folder tree, added dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -15,11 +15,12 @@ gulp.task('sass', function() {
 // Watch Sass & Serve
 gulp.task('serve', ['sass'], function() {
     browserSync.init({
-        server: "./src"  
+        server: "./src",
+
     });
 
     gulp.watch(['src/scss/*.scss'], ['sass']);
-    gulp.watch("src/*.html").on('change', browserSync.reload);
+    gulp.watch("/*.html").on('change', browserSync.reload);
 });
 
 // Default Task
