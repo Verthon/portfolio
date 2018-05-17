10651184b5f92d9c17e2eb5dce5f781a5cd73e89
Verthon <krzysiekksordyl@gmail.com>
2018-05-17 12:13:25 +0200

    major changes to header and nav

--- src/js/main.js
+++ src/js/main.js
@@ -2,3 +2,9 @@ const btn = document.querySelector('.btn-my-work');
 document.addEventListener('load', () => {
   btn.style.opacity = "1";
 });
+
+const menu = document.querySelector('#menu');
+menu.addEventListener('click', () => {
+  const menu = document.querySelector('.menu-container');
+  menu.classList.toggle('menu-container-mobile');
+});
