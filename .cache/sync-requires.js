960330f417f467cb5f3aae1a6f966959cd314c85
Verthon <krzysiekksordyl@gmail.com>
2019-11-22 19:29:15 +0100

    Added new version of readme, new build in public folder

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,7 +5,6 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("/home/jurr/Projects/portfolio/.cache/dev-404-page.js"))),
   "component---src-pages-404-js": hot(preferDefault(require("/home/jurr/Projects/portfolio/src/pages/404.js"))),
   "component---src-pages-index-js": hot(preferDefault(require("/home/jurr/Projects/portfolio/src/pages/index.js")))
 }
--- README.md
+++ README.md
@@ -32,10 +32,3 @@ My first personal portfolio
     ```shell
     gatsby develop
     ```
-
-
-## 💫 Deploy
-
-[![Deploy to Netlify](https://www.netlify.com/img/deploy/button.svg)](https://app.netlify.com/start/deploy?repository=https://github.com/gatsbyjs/gatsby-starter-default)
-
-<!-- AUTO-GENERATED-CONTENT:END -->
