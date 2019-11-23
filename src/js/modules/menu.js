df2f335c69b47fd67b189705c9bb36c3a98ebdf8
Verthon <krzysiekksordyl@gmail.com>
2019-11-23 08:25:31 +0100

    Prepared for merge from dev branch

--- src/js/modules/menu.js
+++
@@ -1,9 +0,0 @@
-const menu = () => {
-  const menu = document.querySelector('#menu');
-  menu.addEventListener('click', () => {
-  const menu = document.querySelector('.menu-container');
-  menu.classList.toggle('menu-container-mobile');
-});
-}
-
-export default menu;
