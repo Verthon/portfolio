502f7c8c5d8d831cfece5c2679de099ff1fb83c8
Verthon <krzysiekksordyl@gmail.com>
2019-11-28 14:40:48 +0100

    Added new build

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,5 @@
+const path = require('path')
+
 module.exports = {
   siteMetadata: {
     title: 'Krzysztof Sordyl Frontend Developer',
@@ -10,7 +12,7 @@ module.exports = {
       resolve: 'gatsby-source-filesystem',
       options: {
         name: 'images',
-        path: `${__dirname}/src/images`
+        path: path.join(__dirname, 'src', 'images')
       }
     },
     'gatsby-transformer-sharp',
