9d99414502e5301d08bc2471476b5db864f0754b
Verthon <krzysiekksordyl@gmail.com>
2020-01-03 21:58:27 +0100

    Added fix for hero svg. Changed about component

--- src/components/About.js
+++ src/components/About.js
@@ -4,10 +4,9 @@ const About = props => {
   return (
     <section className='section about'>
       <p className='container about__description'>
-        I am a web developer living in Bielsko-Biała, Poland. I build websites,
-        particularly, I specialize in frontend development HTML5, SCSS, modern
-        JavaScript. Currently learning more about React, Redux and Node.js. I'm
-        looking for the first job in IT or intern.
+        I am a developer living in Bielsko-Biała, Poland. I build websites and web apps
+        particularly, I specialize in frontend development React.js, SCSS, modern
+        JavaScript. Currently learning Typescript, PostgreSQL and Node.js.
       </p>
     </section>
   )
