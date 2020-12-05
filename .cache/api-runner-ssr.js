9dc3199c5b4085790390bb1a09f9295446f88fa8
Verthon <christopher.sordyl@gmail.com>
2020-12-05 10:20:01 +0100

    feat: add npm audit fixes

--- .cache/api-runner-ssr.js
+++ .cache/api-runner-ssr.js
@@ -1,11 +1,11 @@
 var plugins = [{
-      plugin: require('/home/verthon/Projects/portfolio/node_modules/gatsby-plugin-react-helmet/gatsby-ssr'),
+      plugin: require('C:/Users/jurrp/Desktop/Projects/portfolio/node_modules/gatsby-plugin-react-helmet/gatsby-ssr'),
       options: {"plugins":[]},
     },{
-      plugin: require('/home/verthon/Projects/portfolio/node_modules/gatsby-plugin-manifest/gatsby-ssr'),
+      plugin: require('C:/Users/jurrp/Desktop/Projects/portfolio/node_modules/gatsby-plugin-manifest/gatsby-ssr'),
       options: {"plugins":[],"name":"Krzysztof Sordyl frontend developer from Bielsko Biała, Poland","short_name":"Krzysztof Sordyl portfolio","start_url":"/","background_color":"#663399","theme_color":"#663399","display":"minimal-ui","icon":"src/images/android-chrome-192x192.png"},
     },{
-      plugin: require('/home/verthon/Projects/portfolio/node_modules/gatsby-plugin-offline/gatsby-ssr'),
+      plugin: require('C:/Users/jurrp/Desktop/Projects/portfolio/node_modules/gatsby-plugin-offline/gatsby-ssr'),
       options: {"plugins":[]},
     }]
 // During bootstrap, we write requires at top of this file which looks like:
--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,8 +5,8 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("/home/verthon/Projects/portfolio/.cache/dev-404-page.js"))),
-  "component---src-pages-404-js": hot(preferDefault(require("/home/verthon/Projects/portfolio/src/pages/404.js"))),
-  "component---src-pages-index-js": hot(preferDefault(require("/home/verthon/Projects/portfolio/src/pages/index.js")))
+  "component---cache-dev-404-page-js": hot(preferDefault(require("C:\\Users\\jurrp\\Desktop\\Projects\\portfolio\\.cache\\dev-404-page.js"))),
+  "component---src-pages-404-js": hot(preferDefault(require("C:\\Users\\jurrp\\Desktop\\Projects\\portfolio\\src\\pages\\404.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("C:\\Users\\jurrp\\Desktop\\Projects\\portfolio\\src\\pages\\index.js")))
 }
 
