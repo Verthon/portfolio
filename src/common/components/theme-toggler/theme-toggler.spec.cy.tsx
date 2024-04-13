c96abdccf6017564203d396356ae9c08c28821dc
Verthon <christopher.sordyl@gmail.com>
2024-04-13 08:44:21 +0200

    feat: update crucial dependencies

--- src/common/components/theme-toggler/theme-toggler.spec.cy.tsx
+++ src/common/components/theme-toggler/theme-toggler.spec.cy.tsx
@@ -3,12 +3,14 @@ import ThemeToggler from './theme-toggler'
 
 describe('ThemeToggler', () => {
   it('should by default has system theme selected', () => {
+    //@ts-ignore
     mount(<ThemeToggler />)
 
     cy.contains(/system mode/i).should('be.visible')
   })
 
   it('should allow to change the theme to dark one', () => {
+    //@ts-ignore
     mount(<ThemeToggler />)
 
     cy.get('select').select('Dark mode')
