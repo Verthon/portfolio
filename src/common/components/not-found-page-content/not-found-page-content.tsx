a569904346f6082826e8741bab936173abbdb715
Verthon <christopher.sordyl@gmail.com>
2024-08-03 09:47:31 +0200

    feat: improve headings and refactor the 404 page

--- src/common/components/not-found-page-content/not-found-page-content.tsx
+++ src/common/components/not-found-page-content/not-found-page-content.tsx
@@ -1,21 +1,26 @@
-import { NotFoundImage } from '../not-found-image/not-found-image'
 import { component$ } from '@builder.io/qwik'
 
-import {
-  notFound,
-  title,
-  btn,
-} from './not-found-page-content.module.css'
+import { notFound, title, link, linksWrapper, text } from './not-found-page-content.module.css'
+import { Link } from '@builder.io/qwik-city'
 
 export default component$(() => {
   return (
     <div class={notFound}>
-      <NotFoundImage />
       <h1 class={title}>Sorry, page not found</h1>
-      <p>You just hit a route that doesn&#39;t exist...</p>
-      <a href="/" class={btn}>
-        Go back
-      </a>
+      <p class={text}>You just hit a route that doesn&#39;t exist...</p>
+      <div class={linksWrapper}>
+        <Link href="/" class={link}>
+          go back home
+        </Link>
+
+        <Link href="/" class={link}>
+          visit blog
+        </Link>
+
+        <Link href="/" class={link}>
+          visit dev-bites
+        </Link>
+      </div>
     </div>
   )
 })
