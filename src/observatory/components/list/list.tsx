218f73d47a4bf5479d6cd66b3c8022cb8f70b13c
Verthon <christopher.sordyl@gmail.com>
2025-02-03 16:51:03 +0100

    feat: make sure list is valid html in observatory

--- src/observatory/components/list/list.tsx
+++ src/observatory/components/list/list.tsx
@@ -1,6 +1,6 @@
 import { component$ } from '@builder.io/qwik'
 
-import { ol, li, title, status, preview } from './list.module.css'
+import { ol, li, title, status, preview, link } from './list.module.css'
 import { Link } from '@builder.io/qwik-city'
 
 type ObservatoryItem = {
@@ -18,15 +18,15 @@ export default component$(({ items }: ObservatoryListProps) => {
   return (
     <ol class={ol}>
       {items.map((item) => (
-        <Link href={item.href} key={item.title}>
-          <li class={li}>
+        <li key={item.title} class={li}>
+          <Link href={item.href} class={link}>
             <div class={title}>
               {item.title}
               {item.status && <span class={status}> — {item.status}</span>}
             </div>
             <p class={preview}>{item.shortPreview}</p>
-          </li>
-        </Link>
+          </Link>
+        </li>
       ))}
     </ol>
   )
