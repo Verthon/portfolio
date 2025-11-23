e13ff3baa6b145568ae7191c5a4dbfe16597a9eb
Verthon <christopher.sordyl@gmail.com>
2025-11-23 13:58:55 +0100

    feat: add cookieless mode for all the analytics

--- src/common/components/analytics-script/useInitAnalytics.tsx
+++ src/common/components/analytics-script/useInitAnalytics.tsx
@@ -4,6 +4,7 @@ import posthog from 'posthog-js'
 export const useInitAnalytics = () => {
   useVisibleTask$(() => {
     posthog.init('phc_iV39S2VbjQJAcXSU0fR8bhzwWf3g6nBAqEgMiX9qDFh', {
+      cookieless_mode: 'always',
       api_host: 'https://eu.i.posthog.com',
       defaults: '2025-05-24',
     })
