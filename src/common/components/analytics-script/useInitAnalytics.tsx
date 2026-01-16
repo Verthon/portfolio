1abb9653f08e73004232e4f33c2d047816d20409
Verthon <christopher.sordyl@gmail.com>
2026-01-16 16:03:33 +0100

    feat: reverse proxy for posthog

--- src/common/components/analytics-script/useInitAnalytics.tsx
+++ src/common/components/analytics-script/useInitAnalytics.tsx
@@ -5,8 +5,8 @@ export const useInitAnalytics = () => {
   useVisibleTask$(() => {
     posthog.init('phc_iV39S2VbjQJAcXSU0fR8bhzwWf3g6nBAqEgMiX9qDFh', {
       cookieless_mode: 'always',
-      api_host: 'https://eu.i.posthog.com',
-      defaults: '2025-05-24',
+      api_host: '/ingest',
+      ui_host: 'https://eu.i.posthog.com',
     })
   })
 }
